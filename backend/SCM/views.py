721fbe5c6f191ce1b4089ccb1a33cf2489eebeba
Rajarshi Das <das.rajarshi.riju@gmail.com>
2020-02-07 16:57:14 +0000

    Added @api-view

--- backend/SCM/views.py
+++ backend/SCM/views.py
@@ -11,6 +11,7 @@ def PollutionView(request):
     response = requests.get('http://erc.epa.ie/real-time-air/www/aqindex/aqih_json.php')
     return HttpResponse(json.dumps(response.json()), content_type="application/json")
 
+@api_view()
 def DublinBikeView(request):
     response = requests.get('https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=77cf7ab00377c7f4cc621765273db0e7daf18f82')
     return HttpResponse(json.dumps(response.json()), content_type="application/json")
