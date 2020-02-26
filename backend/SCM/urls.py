422a9ea9c7262107726b352ec44aeaed2bf2fcaa
longeo <longeo@tcd.ie>
2020-02-26 12:36:17 +0000

    url changes

--- backend/SCM/urls.py
+++ backend/SCM/urls.py
@@ -2,25 +2,13 @@ from SCM.views import   (
                             PollutionView,
                             DublinBikeView,
                             push_notify,
-<<<<<<< HEAD
-=======
-                            EventView,
-                            DublinBikeChartView,
-                            EventView,
-                            DublinBusView
->>>>>>> 696f87234febfc45fc5e74f594c5f9b6a2f6e571
+
                         )
 from django.urls import path
 
 app_name = "SCM"
 urlpatterns = [
-<<<<<<< HEAD
-    path('pollution/', PollutionView),
-    path('dublinbike/', DublinBikeView),
-    path('notification/', push_notify),
 
-]
-=======
     path('pollution/', PollutionView , name='PollutionView'),
     path('dublinbike/', DublinBikeView, name='DublinBikeView'),
     path('notification/', push_notify),
@@ -28,4 +16,3 @@ urlpatterns = [
     path('event/', EventView, name='EventView'),
     path('dublinbus/', DublinBusView)
 ]
->>>>>>> 696f87234febfc45fc5e74f594c5f9b6a2f6e571
