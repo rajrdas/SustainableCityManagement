b8f6ca4ee2789f8ef0e500dfaf910ae3229590e0
rohanbhutani1993 <rohanbhutani1993@gmail.com>
2020-03-11 15:01:28 +0000

    Adding spinner for all tables

--- frontend/src/landingcomponents/LandingPage.js
+++ frontend/src/landingcomponents/LandingPage.js
@@ -1,75 +1,66 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col, Card, Navbar, Form} from 'react-bootstrap';
+import { Row, Col, Card, Navbar, Form } from 'react-bootstrap';
 import LoginModal from './LoginModal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Timeline } from 'react-twitter-widgets'
 import "../../node_modules/video-react/dist/video-react.css";
 
-
 class LandingPage extends React.Component {
-
-
     render() {
-
         return (
             <div>
-            <Row>
-                <Col>
-                    <Navbar bg="transparent" expand="lg">
-                        <Navbar.Brand href="#home" inline>
-                            <h1>Sustainable City Management</h1>
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Form inline className="ml-auto">
-                                <LoginModal />
-                            </Form>
-                        </Navbar.Collapse>
-                    </Navbar>
-                </Col>
-            </Row>
-            <Row style={{ marginTop: -8 }}>
-                <Col>
-                    <div>
-                        <iframe src='https://www.youtube.com/embed/Rk8VibKA4Ws?autoplay=1&controls=0&autohide=1&disablekb=1&loop=1&modestbranding=1'
-                            allow='autoplay; encrypted-media'
-                            allowFullScreen='allowFullScreen'
-                            title='video'
-                            style={{
-                                height:'42.25vw', width:'100vw',
-                            }}
-                        />
-                    </div>
-                </Col>
-            </Row>
-            <Row style={{ marginLeft: 10 }}>
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Body>
-                            <Timeline
-                                dataSource={{
-                                    sourceType: 'profile',
-                                    screenName: 'DCCTraffic'
-                                }}
-                                options={{
-                                    username: 'DCCTraffic',
-                                    height: '400',
-                                    width: '1200'
+                <Row>
+                    <Col>
+                        <Navbar bg="transparent" expand="lg">
+                            <Navbar.Brand href="#home">
+                                <h1>Sustainable City Management</h1>
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Form inline className="ml-auto">
+                                    <LoginModal />
+                                </Form>
+                            </Navbar.Collapse>
+                        </Navbar>
+                    </Col>
+                </Row>
+                <Row style={{ marginTop: -8 }}>
+                    <Col>
+                        <div>
+                            <iframe src='https://www.youtube.com/embed/Rk8VibKA4Ws?autoplay=1&controls=0&autohide=1&disablekb=1&loop=1&modestbranding=1'
+                                allow='autoplay; encrypted-media'
+                                allowFullScreen='allowFullScreen'
+                                title='video'
+                                style={{
+                                    height: '42.25vw', width: '100vw',
                                 }}
                             />
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-        </div>
-
+                        </div>
+                    </Col>
+                </Row>
+                <Row style={{ marginLeft: 10 }}>
+                    <Col>
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Timeline
+                                    dataSource={{
+                                        sourceType: 'profile',
+                                        screenName: 'DCCTraffic'
+                                    }}
+                                    options={{
+                                        username: 'DCCTraffic',
+                                        height: '400',
+                                        width: '1200'
+                                    }}
+                                />
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
         );
     }
-
 };
-
-
-
 
 export default LandingPage;
--- frontend/src/landingcomponents/LoginModal.js
+++ frontend/src/landingcomponents/LoginModal.js
@@ -18,7 +18,6 @@ class LoginForm extends Component {
     }
   }
 
-
   renderField = ({ input, label, type, meta: { touched, error } }) => {
     return (
       <div className={`field ${touched && error ? 'error' : ''}`}>
@@ -60,49 +59,49 @@ class LoginForm extends Component {
       return <Redirect to='/' />;
     }
     return (
-  <div>
-    <Button variant="outline-dark" onClick={this.open.bind(this)}>Login/Register</Button>
+      <div>
+        <Button variant="outline-dark" onClick={this.open.bind(this)}>Login/Register</Button>
 
-    <Modal size="lg" show={this.state.showModal} onHide={this.close.bind(this)}>
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Login/Register Form
+        <Modal size="lg" show={this.state.showModal} onHide={this.close.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Login/Register Form
         </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-      <div className='ui container'>
-        <div className='ui segment'>
-          <form
-            onSubmit={this.props.handleSubmit(this.onSubmit)}
-            className='ui form'
-          >
-            <Field
-              name='username'
-              type='text'
-              component={this.renderField}
-              label='Username'
-            />
-            <Field
-              name='password'
-              type='password'
-              component={this.renderField}
-              label='Password'
-            />
-            <Field
-              name='non_field_errors'
-              type='hidden'
-              component={this.hiddenField}
-            />
-            <button className='ui primary button'>Login</button>
-          </form>
-          <p style={{ marginTop: '1rem' }}>
-            Don't have an account? <Link to='/register'>Register</Link>
-          </p>
-        </div>
+          </Modal.Header>
+          <Modal.Body>
+            <div className='ui container'>
+              <div className='ui segment'>
+                <form
+                  onSubmit={this.props.handleSubmit(this.onSubmit)}
+                  className='ui form'
+                >
+                  <Field
+                    name='username'
+                    type='text'
+                    component={this.renderField}
+                    label='Username'
+                  />
+                  <Field
+                    name='password'
+                    type='password'
+                    component={this.renderField}
+                    label='Password'
+                  />
+                  <Field
+                    name='non_field_errors'
+                    type='hidden'
+                    component={this.hiddenField}
+                  />
+                  <button className='ui primary button'>Login</button>
+                </form>
+                <p style={{ marginTop: '1rem' }}>
+                  Don't have an account? <Link to='/register'>Register</Link>
+                </p>
+              </div>
+            </div>
+          </Modal.Body>
+        </Modal>
       </div>
-    </Modal.Body>
-  </Modal>
-</div>
     );
   }
 }
