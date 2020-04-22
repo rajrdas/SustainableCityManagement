04d3e95e46ae17da012c0cdb6a29072c5ef5ed85
rohanbhutani1993 <rohanbhutani1993@gmail.com>
2020-04-22 14:42:07 +0100

    removing redundant code

--- frontend/src/components/BikeAvailabilityChart.js
+++ frontend/src/components/BikeAvailabilityChart.js
@@ -3,19 +3,14 @@ import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 
 const BikeAvailabilityChart = (props) => {
-    var i =0 
-    var arr =[]
-    for(i=0; i <props.bikeCount.length; i++)
-    {
-        arr.push(props.bikeCount[i])
-    }
+    
     const options = {
         title: {
             text: 'Bikes Available at ' + props.stop + ' stop'
         },
         series: [{
             name: 'Bikes Available',
-            data: arr
+            data: props.bikeCount
         }],
         yAxis: [{
             title : {
